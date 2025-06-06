26d37b5c14835c98c6f60c3295a8619726f81773
matheusbarcc <matheusbarcon@gmail.com>
2025-06-06 14:41:10 -0300

    ci: add railway deploy config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,8 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import { env } from '@/env'
-import * as schema from './schema'
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { env } from "@/env";
+import * as schema from "./schema";
 
-const client = postgres(env.DB_URL)
+const client = postgres(env.DATABASE_URL);
 
-export const db = drizzle(client, { schema })
+export const db = drizzle(client, { schema });
